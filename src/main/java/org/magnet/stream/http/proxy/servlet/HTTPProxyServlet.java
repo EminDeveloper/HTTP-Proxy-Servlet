a40f162ef560ca6a2ccfd81eeb4a48328a3e36be
Emin <emin.f.mammadov@gmail.com>
2022-12-03 12:14:31 +0400

    first

---
+++ src/main/java/org/magnet/stream/http/proxy/servlet/HTTPProxyServlet.java
@@ -0,0 +1,12 @@
+package org.magnet.stream.http.proxy.servlet;
+
+/**
+ *
+ * @author Emin
+ */
+public class HTTPProxyServlet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
